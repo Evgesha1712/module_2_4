c9a17b646ee403a33f35cb14ee6cd650d972f7ea
Evgesha <kydinova1712@gmail.com>
2024-12-10 16:52:32 +0300

    Add files via upload

---
+++ module_2_4.py
@@ -0,0 +1,15 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = list
+not_primes = list
+is_prime = True
+
+for i in (numbers[::]):
+    for j in (numbers[::]):
+        if i // j:
+            primes.insert()
+        else:
+            not_primes = i
+
+
+print(primes)
+print(not_primes)
